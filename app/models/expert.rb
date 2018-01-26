a49683e9c8c736f7fe7ac74f4e980ba3a8f4dd19
Albert <oscaralberto@gmail.com>
2018-01-26 15:09:43 -0700

    se crea la relacion de uno a muchos de HasSpecialty con Expert y Specialty

--- app/models/expert.rb
+++ app/models/expert.rb
@@ -1,2 +1,4 @@
 class Expert < ApplicationRecord
+  has_many :has_specialties
+  has_many :specialties, through: :has_specialties
 end
--- app/models/specialty.rb
+++ app/models/specialty.rb
@@ -1,2 +1,4 @@
 class Specialty < ApplicationRecord
+  has_many :has_specialties
+  has_many :experts, through: :has_specialties
 end
